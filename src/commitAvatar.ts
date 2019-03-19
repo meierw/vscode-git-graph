fefa621c4e13ff4a8c6546c4e635154ef7abc6df
Valters Meirēns <valters.meirens@tieto.com>
2019-03-19 14:35:42 +0200

    WIP: Update CommitAvatar with Research Notes

--- src/commitAvatar.ts
+++ src/commitAvatar.ts
@@ -1,47 +1,21 @@
 import crypto = require('crypto');
-import fs = require('fs');
 import http = require('http');
 
 export class CommitAvatar {
-
-  /**
-   * Searches through the `services` (in the order they are specified)
-   * for the users avatar picture.
-   * @param username - Will be used for querying everything, except `'gravatar'`.
-   * @param email - Will be used for querying `'gravatar'`.
-   * @param services - Possible values: `['github', 'gitlab', 'gravatar']`.
-   * Default: `['github', 'gravatar']`.
-   * @returns A `Promise` with the first found avatar, or with `null` if nothing found.
-   */
-  public static async retrieve(username: string, email = '', services = ['github', 'gravatar']): Promise<string | null> {
-    let result: string | null = null;
-
-    // Could alternatively get all the promises at once,
-    // so they start doing http requests in parallel,
-    // but that probably will just saturate the network too much,
-    // when the first service most likely will have the needed result.
-    for (let i in services) {
-      let serviceProper = services[i].replace(/\w\S*/g,
-        function(s){return s.charAt(0).toUpperCase() + s.substr(1).toLowerCase();}
-      );
-      if (serviceProper === 'Gravatar') {
-        result = await CommitAvatar.retrieveFromGravatar(email);
-      } else {
-        let retrieveMethod = (CommitAvatar as any)[`retrieveFrom${serviceProper}`];
-        if (retrieveMethod) result = await retrieveMethod(username);
-      }
-      
-      if (result !== null) break;
-    }
-
-    return result;
-  }
-
-  private static async retrieveFromGithub(email: string): Promise<string | null> {
+  public static async fromGithub(commitHash: string): Promise<string | null> {
+    // Example where I pushed with known email: https://api.github.com/repos/meierw/buildbot/commits/6e14c159864306fc571571d7163cb9ebc4c8f8c7
+    // Example where I pushed with unknown email: https://api.github.com/repos/meierw/vscode-git-graph/commits/fc0bef9af8e556a304f82719ed389d1d95629ad5
     return 'githubBase64';
   }
 
-  private static async retrieveFromGravatar(email: string, identicon = true): Promise<string | null> {
+  public static async fromGitlab(commitHash: string): Promise<string | null> {
+    // Gitlab user can be found exactly by email
+    // Need to login with free account first though
+    // https://gitlab.com/api/v4/users?search=valters.meirens@gmail.com
+    return 'gitlabBase64';
+  }
+
+  public static async fromGravatar(email: string, identicon = true): Promise<string | null> {
     let hash = crypto.createHash('md5').update(email).digest('hex');
 
     return new Promise((resolve) => {
@@ -57,3 +31,16 @@ export class CommitAvatar {
     });
   }
 }
+
+/* Research notes:
+Github user can't be found by email like in Gitlab,
+unless the user has made his email public. So the only
+way, to find the github profile properly, is to check
+the commit info, which contains the "author" field.
+
+The minimum steps that we'll need to take for Github are:
+  * Create a local user definition for a specific `user.name` and `user.email` combo
+  * Get the lastest commit by this local user
+  * Check out Gravatar if nothing relevant was found on Github
+  * Cache it so that this process is not repeated for every commit made by this local user
+*/
