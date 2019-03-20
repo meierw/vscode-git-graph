75a6dbf8881672d69d98ff10ff8ef51d2a313124
Valters Meirēns <valters.meirens@tieto.com>
2019-03-20 11:18:50 +0200

    WIP: Add initial CommitAvatar.fromGithub implementation

--- src/commitAvatar.ts
+++ src/commitAvatar.ts
@@ -1,14 +1,48 @@
 import crypto = require('crypto');
 import http = require('http');
+import https = require('https');
+import url = require('url');
 
 export class CommitAvatar {
-  public static async fromGithub(commitHash: string): Promise<string | null> {
+  public static async fromGithub(owner: string, repo: string, commitHash: string): Promise<string | null> {
     // Example where I pushed with known email: https://api.github.com/repos/meierw/buildbot/commits/6e14c159864306fc571571d7163cb9ebc4c8f8c7
     // Example where I pushed with unknown email: https://api.github.com/repos/meierw/vscode-git-graph/commits/fc0bef9af8e556a304f82719ed389d1d95629ad5
-    return 'githubBase64';
+    
+    return new Promise((resolve) => {
+      https.get({
+        hostname: 'api.github.com',
+        path: `/repos/${owner}/${repo}/commits/${commitHash}`,
+        headers: { 'User-Agent': 'Git Graph extension for Visual Studio Code' },
+        agent: false
+      }, (res: http.IncomingMessage) => {
+        let commitJson = '';
+        res.on('data', (chunk: Buffer) => { commitJson += chunk; });
+        res.on('end', () => {
+          let commit: any = JSON.parse(commitJson);
+          let avatarUrl: any = commit.author && commit.author.avatar_url;
+          if (!avatarUrl) {
+            resolve(null); 
+          } else {
+            avatarUrl = url.parse(avatarUrl);
+            https.get({
+              hostname: avatarUrl.hostname,
+              path: avatarUrl.path,
+              agent: false
+            }, (res: http.IncomingMessage) => {
+              let imageBufferArray: Buffer[] = [];
+              res.on('data', (chunk: Buffer) => { imageBufferArray.push(chunk); });
+              res.on('end', () => {
+                let image: Buffer = Buffer.concat(imageBufferArray);
+                resolve(image.toString('base64') || null);
+              });
+            });
+          }
+        });
+      });
+    });
   }
 
-  public static async fromGitlab(commitHash: string): Promise<string | null> {
+  public static async fromGitlab(email: string): Promise<string | null> {
     // Gitlab user can be found exactly by email
     // Need to login with free account first though
     // https://gitlab.com/api/v4/users?search=valters.meirens@gmail.com
@@ -19,13 +53,16 @@ export class CommitAvatar {
     let hash = crypto.createHash('md5').update(email).digest('hex');
 
     return new Promise((resolve) => {
-      http.get({
+      https.get({
         hostname: 'gravatar.com',
         path: `/avatar/${hash}${identicon ? '?d=identicon' : ''}`,
         agent: false
       }, (res: http.IncomingMessage) => {
-        res.on('data', (data: Buffer) => {
-          resolve(data.toString('base64') || null);
+        let imageBufferArray: Buffer[] = [];
+        res.on('data', (chunk: Buffer) => { imageBufferArray.push(chunk); });
+        res.on('end', () => {
+          let image: Buffer = Buffer.concat(imageBufferArray);
+          resolve(image.toString('base64') || null);
         });
       });
     });
